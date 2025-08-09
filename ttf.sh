12202df14780878f6faaa289d87927a340f8cb56
FerNandoGD24 <fernando.gatica.dominguez@outlook.com>
2025-08-09 19:45:55 -0400

    Add files via upload

---
+++ ttf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#instalacion de fuentes
+    sudo cat ttfp_* > ttf_u.tar.xz
+    sudo xz -d ttf_u.tar.xz
+    sudo mkdir -p /usr/share/fonts
+    sudo mkdir -p /usr/local/share/fonts
+    sudo mkdir -p ~/.fonts 
+    sudo tar -xvf ttf_u.tar -C /usr/share/fonts
+    sudo tar -xvf ttf_u.tar -C /usr/local/share/fonts
+    sudo tar -xvf ttf_u.tar -C ~/.fonts
