14d01fcce26463738af9a8c848ed82e3872c4f10
FerNandoGD24 <fernando.gatica.dominguez@outlook.com>
2025-08-09 20:20:39 -0400

    ttf.sh

--- ttf.sh
+++ ttf.sh
@@ -6,10 +6,15 @@
     echo ::{::::::::::::::::::}::
 #instalacion de fuentes
     sudo cat ttfp_* > ttf_u.tar.xz
+    sleep 5
     sudo xz -d ttf_u.tar.xz
+    sleep 5
     sudo mkdir -p /usr/share/fonts
     sudo mkdir -p /usr/local/share/fonts
-    sudo mkdir -p ~/.fonts 
+    sudo mkdir -p ~/.fonts
+    sleep 5
     sudo tar -xvf ttf_u.tar -C /usr/share/fonts
+    sleep 5
     sudo tar -xvf ttf_u.tar -C /usr/local/share/fonts
+    sleep 5
     sudo tar -xvf ttf_u.tar -C ~/.fonts
